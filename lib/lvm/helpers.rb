35ad91f0c3f375371c701aa9b2ceb4f6f89dff08
Matt Palmer <mpalmer@hezmatt.org>
2014-09-03 12:43:47 +1000

    Fix htonq to actually be network byte order

--- lib/lvm/helpers.rb
+++ lib/lvm/helpers.rb
@@ -9,7 +9,7 @@ module LVM::Helpers
 	def htonq val
 		# This won't work on a nUxi byte-order machine, but if you have one of
 		# those, I'm guessing you've got bigger problems
-		big_endian? ? ([val].pack("Q").reverse.unpack("Q").first) : val
+		big_endian? ? val : ([val].pack("Q").reverse.unpack("Q").first)
 	end
 
 	def ntohq val
